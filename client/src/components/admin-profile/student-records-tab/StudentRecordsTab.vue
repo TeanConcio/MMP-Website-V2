b75e7c7d1214c7355aa2c22ad32b067f3baf2e40
Tean Concio <tean_concio@dlsu.edu.ph>
2024-03-10 04:02:16 +0800

    Added download buttons and planned logic in StudentRecordsTab

--- client/src/components/admin-profile/student-records-tab/StudentRecordsTab.vue
+++ client/src/components/admin-profile/student-records-tab/StudentRecordsTab.vue
@@ -5,6 +5,8 @@ import { useCredentialsStore } from "../../../store/store";
 import ModuleCard from "../../common/ModuleCard.vue";
 import LoadingSpinner from "../../common/LoadingSpinner.vue";
 import Dropdown from "../../common/Dropdown.vue";
+// Helpers
+import { downloadZIP } from "../../../util/helpers";
 // Emits
 defineEmits(["select-module"]);
 </script>
@@ -51,6 +53,27 @@ defineEmits(["select-module"]);
                 </button>
             </div>
         </div>
+        <div
+            class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 px-7 py-5 mb-5"
+        >
+            <h2 class="text-2xl font-semibold mb-1">Download</h2>
+            <div class="mb-5">
+                <button
+                    @click="downloadYearModules"
+                    type="button"
+                    class="w-full sm:w-auto ml-auto mr-10 md:px-10 px-3 py-3 mt-5 text-base font-medium text-center text-white bg-highlight rounded-lg hover:bg-highlight_hover"
+                >
+                    Download Year Module Student Records (.zip)
+                </button>
+                <button
+                    @click="downloadAllModules"
+                    type="button"
+                    class="w-full sm:w-auto ml-auto mr-10 md:px-10 px-3 py-3 mt-5 text-base font-medium text-center text-white bg-highlight rounded-lg hover:bg-highlight_hover"
+                >
+                    Download All Module Student Records (.zip)
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -81,6 +104,32 @@ export default {
                 .then(({ data }) => {
                     // Store data
                     this.modulesArray = data;
+                })
+                // If unsuccessful
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        async downloadYearModules() {
+            await this.$axios
+                .get(`/download/modules/${this.filterYear}`)
+                // If successful
+                .then(({ data }) => {
+                    // Download data
+                    downloadZIP(data, `${this.filterYear} Module Student Records Data.zip`);
+                })
+                // If unsuccessful
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        async downloadAllModules() {
+            await this.$axios
+                .get(`/download/modules`)
+                // If successful
+                .then(({ data }) => {
+                    // Download data
+                    downloadZIP(data, "All Module Student Records Data.zip");
                 })
                 // If unsuccessful
                 .catch((error) => {
