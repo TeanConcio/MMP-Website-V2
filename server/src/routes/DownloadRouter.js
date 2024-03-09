7f4078a2e4e1745a68abd649fe5848d269a4ac56
Tean Concio <tean_concio@dlsu.edu.ph>
2024-03-10 04:30:08 +0800

    Added download database as zipped csvs functionality

--- server/src/routes/DownloadRouter.js
+++ server/src/routes/DownloadRouter.js
@@ -9,11 +9,68 @@ import { exclude, allowed } from "../utils/helpers.js";
 const DownloadRouter = express.Router();
 
 /* Helpers */
+const excludePasswordDates = (object) => {
+    return exclude(object, ["password", "created_at", "updated_at"]);
+};
 
 /* Controllers */
 
 /* GET Endpoints */
 
+// Download All Tables as Zipped CSVs
+DownloadRouter.get("/all", async (req, res) => {
+    if (!allowed(req.permission, [3])) {
+        res.status(401).send({ error: "You are not authorized to access this" });
+        return;
+    }
+
+    try {
+        const zip = new JSZip();
+
+        // Get All Tables
+        let database = {
+            students: await prisma.Students.findMany({}),
+            bills: await prisma.Bills.findMany({}),
+            admins: await prisma.Admins.findMany({}),
+            teachers: await prisma.Teachers.findMany({}),
+            payments: await prisma.Payments.findMany({}),
+            modules: await prisma.Modules.findMany({}),
+            module_enrollments: await prisma.Module_Enrollments.findMany({}),
+            tor_requests: await prisma.TOR_Requests.findMany({}),
+            module_details: await prisma.Module_Details.findMany({}),
+            module_names: await prisma.Module_Names.findMany({}),
+            tickets: await prisma.Tickets.findMany({}),
+        };
+
+        // Exclude password and dates
+        database = Object.fromEntries(
+            Object.entries(database).map(([key, value]) => [key, value.map(excludePasswordDates)])
+        );
+
+        // For each table, export as CSV
+        for (const [key, value] of Object.entries(database)) {
+            exclude(value, ["password", "created_at", "updated_at"]);
+            try {
+                if (value) {
+                    // Check if value array is not empty
+                    zip.file(`${key}.csv`, parseToCSV(value));
+                } else {
+                    console.log(`No ${key} in the database`);
+                }
+            } catch (err) {
+                console.error("Problem exporting " + key + ": " + err);
+            }
+        }
+
+        const blob = await zip.generateAsync({ type: "base64" });
+        console.log(`ADMIN [${req.user.user_id}] EXPORTED the database`);
+
+        res.send(blob);
+    } catch (error) {
+        // Return error
+        res.status(404).send({ error: error.message });
+    }
+});
 
 // Export Router
 export default DownloadRouter;
