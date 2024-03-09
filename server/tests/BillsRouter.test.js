86fb6babe373564facea14eb79a2c07928bce41e
Tean Concio <tean_concio@dlsu.edu.ph>
2024-03-10 06:56:15 +0800

    Fixed Unit Tests for Bills and Payments Router

--- server/tests/BillsRouter.test.js
+++ server/tests/BillsRouter.test.js
@@ -11,27 +11,24 @@ describe("BillsRouter Helper Functions", () => {
     describe('generateBillNo Function', () => {
         // Test case for normal increment of 9-digit segment
         it('increment the 9-digit segment within the limit', async () => {
-          const castedSegments = [1, 999999998];
 
-          const result = await generateBillNo(castedSegments);
+          const result = await generateBillNo(1, 999999998);
 
           expect(result).toBe('1-999999999');
         });
         
         // Test case for rollover to the next 1-digit segment
         it('increment the 9-digit segment and rollover to the next 1-digit segment', async () => {
-          const castedSegments = [8, 999999999];
 
-          const result = await generateBillNo(castedSegments);
+          const result = await generateBillNo(8, 999999999);
 
           expect(result).toBe('9-000000000');
         });
         
         // Test case for exceeding the limits
         it('throw an error when both segments are at their limits', async () => {
-          const castedSegments = [9, 999999999];
 
-          await expect(generateBillNo(castedSegments)).rejects.toThrow('Bill Number Overflow');
+          await expect(generateBillNo(9, 999999999)).rejects.toThrow('Bill Number Overflow');
         });
     });
 });
--- server/tests/PaymentsRouter.test.js
+++ server/tests/PaymentsRouter.test.js
@@ -12,27 +12,24 @@ describe("PaymentsRouter Helper Functions", () => {
 
         // Test case for normal increment of 9-digit segment
         it('increments the 9-digit segment within the limit', async () => {
-            const castedSegments = [1, 999999998];
 
-            const result = await generateOR(castedSegments);
+            const result = await generateOR(1, 999999998);
 
             expect(result).toBe('1-999999999');
         });
 
         // Test case for rollover to the next 1-digit segment
         it('increments the 9-digit segment and rolls over to the next 1-digit segment', async () => {
-            const castedSegments = [8, 999999999];
 
-            const result = await generateOR(castedSegments);
+            const result = await generateOR(8, 999999999);
 
             expect(result).toBe('9-000000000');
         });
 
         // Test case for exceeding the limits
         it('throws an error when both segments are at their limits', async () => {
-            const castedSegments = [9, 999999999];
 
-            await expect(generateOR(castedSegments)).rejects.toThrow('OR Number Overflow');
+            await expect(generateOR(9, 999999999)).rejects.toThrow('OR Number Overflow');
         });
 
 
