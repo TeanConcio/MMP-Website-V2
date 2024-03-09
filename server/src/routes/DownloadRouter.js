736e7b160882507cad03bc4cdc6e23e5bc5c177f
Tean Concio <tean_concio@dlsu.edu.ph>
2024-03-10 05:35:35 +0800

    Added download student data as zipped csv in DownloadRouter

--- server/src/routes/DownloadRouter.js
+++ server/src/routes/DownloadRouter.js
@@ -329,5 +329,124 @@ DownloadRouter.get("/modules/:school_year", async (req, res) => {
     }
 });
 
+// Download All Data of a Student as Zipped CSVs
+DownloadRouter.get("/student/:student_id", async (req, res) => {
+    if (!allowed(req.permission, [3])) {
+        res.status(401).send({ error: "You are not authorized to access this" });
+        return;
+    }
+
+    try {
+        const student_id = req.params.student_id;
+
+        const zip = new JSZip();
+
+        // Get All Tables
+        let student_data = {
+            student: await prisma.Students.findUnique({
+                where: {
+                    student_id: student_id,
+                },
+            }),
+            module_enrollments: await prisma.Module_Enrollments.findMany({
+                where: {
+                    student_id: student_id,
+                },
+                select: {
+                    status: true,
+                    grade: true,
+                    no_of_absences: true,
+                    remarks: true,
+                    module: {
+                        select: {
+                            school_year: true,
+                            session_1: true,
+                            session_2: true,
+                            teacher: {
+                                select: {
+                                    first_name: true,
+                                    middle_name: true,
+                                    last_name: true,
+                                },
+                            },
+                            details: {
+                                select: {
+                                    module_name: true,
+                                    program: true,
+                                },
+                            },
+                        },
+                    },
+                },
+                orderBy: {
+                    module: {
+                        module_name: "asc",
+                    },
+                },
+            }),
+        };
+
+        // Format Student Data
+        student_data.student = exclude(student_data.student, [
+            "password",
+            "created_at",
+            "updated_at",
+        ]);
+
+        // Format Module Enrollments
+        student_data.module_enrollments.forEach((enrollment) => {
+            enrollment.module_name = enrollment.module.details.module_name;
+            enrollment.school_year = enrollment.module.school_year;
+            enrollment.program = enrollment.module.details.program;
+            enrollment.teacher_first_name = enrollment.module.teacher.first_name;
+            enrollment.teacher_middle_name = enrollment.module.teacher.middle_name;
+            enrollment.teacher_last_name = enrollment.module.teacher.last_name;
+            enrollment.session_1 = enrollment.module.session_1;
+            enrollment.session_2 = enrollment.module.session_2;
+            delete enrollment.module;
+        });
+        // Reorder Module Enrollment Columns
+        student_data.module_enrollments = student_data.module_enrollments.map((enrollment) => {
+            return {
+                module_name: enrollment.module_name,
+                school_year: enrollment.school_year,
+                program: enrollment.program,
+                teacher_first_name: enrollment.teacher_first_name,
+                teacher_middle_name: enrollment.teacher_middle_name,
+                teacher_last_name: enrollment.teacher_last_name,
+                session_1: enrollment.session_1,
+                session_2: enrollment.session_2,
+                status: enrollment.status,
+                grade: enrollment.grade,
+                no_of_absences: enrollment.no_of_absences,
+                remarks: enrollment.remarks,
+            };
+        });
+
+        // For each table, export as CSV
+        for (const [key, value] of Object.entries(student_data)) {
+            try {
+                // If value is an array and is empty
+                if (Array.isArray(value) && value.length === 0) {
+                    console.log(`No ${key} for ${student_id}`);
+                } else {
+                    // Check if value array is not empty
+                    zip.file(`${key}.csv`, parseToCSV(value));
+                }
+            } catch (err) {
+                console.error("Problem exporting " + key + ": " + err);
+            }
+        }
+
+        const blob = await zip.generateAsync({ type: "base64" });
+        console.log(`ADMIN [${req.user.user_id}] EXPORTED the database`);
+
+        res.send(blob);
+    } catch (error) {
+        // Return error
+        res.status(404).send({ error: error.message });
+    }
+});
+
 // Export Router
 export default DownloadRouter;
